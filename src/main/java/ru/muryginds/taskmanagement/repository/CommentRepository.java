86ec6194309246ef928ffcca26800492f40847bb
muryginds <muryginds@gmail.com>
2023-12-13 20:49:43 +0300

    repositories added

---
+++ src/main/java/ru/muryginds/taskmanagement/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package ru.muryginds.taskmanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.muryginds.taskmanagement.entity.Comment;
+import ru.muryginds.taskmanagement.entity.Task;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByTask(Task task);
+
+}
---
+++ src/main/java/ru/muryginds/taskmanagement/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package ru.muryginds.taskmanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.muryginds.taskmanagement.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
