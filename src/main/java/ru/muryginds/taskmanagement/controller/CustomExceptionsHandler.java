2a5cd7894d1f8915ff5310767679ca3f3719082d
muryginds <muryginds@gmail.com>
2023-12-18 22:13:01 +0300

    ErrorResponseDTO added + exceptionHandler fixed

--- src/main/java/ru/muryginds/taskmanagement/controller/CustomExceptionsHandler.java
+++ src/main/java/ru/muryginds/taskmanagement/controller/CustomExceptionsHandler.java
@@ -2,20 +2,19 @@ package ru.muryginds.taskmanagement.controller;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.validation.ConstraintViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import ru.muryginds.taskmanagement.dto.response.ErrorResponseDTO;
 import ru.muryginds.taskmanagement.exception.TaskManagerException;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class CustomExceptionsHandler {
@@ -26,45 +25,47 @@ public class CustomExceptionsHandler {
             MissingServletRequestParameterException.class,
             ExpiredJwtException.class
     })
-    public ResponseEntity<Object> handleCustomExceptions(Exception e) {
-        return ResponseEntity.badRequest().body(getResponse(e));
+    public ResponseEntity<ErrorResponseDTO> handleCustomExceptions(Exception e) {
+        return ResponseEntity.badRequest().body(prepareErrorResponse(e));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleException(Exception e) {
-        return ResponseEntity.internalServerError().body(getResponse(e));
+    public ResponseEntity<ErrorResponseDTO> handleException(Exception e) {
+        return ResponseEntity.internalServerError().body(prepareErrorResponse(e));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleArgumentNotValidationException(MethodArgumentNotValidException e) {
-        Map<String, String> errors = new HashMap<>();
-        e.getBindingResult().getAllErrors().forEach((error) -> {
+    public ResponseEntity<ErrorResponseDTO> handleArgumentNotValidationException(MethodArgumentNotValidException e) {
+        List<String> errors = new ArrayList<>();
+        e.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
+            errors.add(String.format("%s - %s", fieldName, errorMessage));
         });
-        return ResponseEntity.badRequest().body(errors);
+        return ResponseEntity.badRequest().body(prepareErrorResponse(errors));
     }
 
     @ExceptionHandler(HandlerMethodValidationException.class)
-    public ResponseEntity<Object> handleMethodValidationException(HandlerMethodValidationException e) {
-        Map<String, String> errors = new HashMap<>();
+    public ResponseEntity<ErrorResponseDTO> handleMethodValidationException(HandlerMethodValidationException e) {
+        List<String> errors = new ArrayList<>();
         e.getAllValidationResults().forEach(error -> {
             String fieldName = error.getMethodParameter().getParameterName();
             error.getResolvableErrors().forEach(message ->
-                    errors.put(fieldName, message.getDefaultMessage()));
+                    errors.add(String.format("%s - %s", fieldName, message.getDefaultMessage()))
+            );
         });
-        return ResponseEntity.badRequest().body(errors);
+        return ResponseEntity.badRequest().body(prepareErrorResponse(errors));
     }
 
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<Object> handleBadCredentialsException(Exception e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(getResponse(e));
+    private ErrorResponseDTO prepareErrorResponse(Exception e) {
+        return ErrorResponseDTO.builder()
+                .errors(List.of(e.getLocalizedMessage()))
+                .build();
     }
 
-    private HashMap<Object, Object> getResponse(Exception e) {
-        var errorMap = new HashMap<>();
-        errorMap.put("Ошибка", e.getLocalizedMessage());
-        return errorMap;
+    private ErrorResponseDTO prepareErrorResponse(List<String> list) {
+        return ErrorResponseDTO.builder()
+                .errors(list)
+                .build();
     }
 }
