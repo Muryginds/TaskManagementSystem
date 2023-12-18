872e622967505c8608465dbed2d66f0c6fb28a49
muryginds <muryginds@gmail.com>
2023-12-18 18:19:42 +0300

    mapper token field ignoring added

--- src/main/java/ru/muryginds/taskmanagement/mapstruct/UserMapper.java
+++ src/main/java/ru/muryginds/taskmanagement/mapstruct/UserMapper.java
@@ -1,10 +1,12 @@
 package ru.muryginds.taskmanagement.mapstruct;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.muryginds.taskmanagement.dto.response.UserDTO;
 import ru.muryginds.taskmanagement.entity.User;
 
 @Mapper
 public interface UserMapper {
+    @Mapping(target = "token", ignore = true)
     UserDTO userToUserDTO(User user);
 }
